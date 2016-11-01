dcec3fa2cc097fa00ad17ea0509a00843b4f4089
LucienCamuglia <patatezarbie@gmail.com>
2016-11-01 11:09:18 +0100

    Creation du projet

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="./css/style.css" rel="stylesheet" />
+
+        <!-- game component -->
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script src="./js/jcanvas.js" type="text/javascript"></script>
+        <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">        
+        <script src="./js/mexican.js" type="text/javascript"></script>
+        <script src="./js/bullet.js" type="text/javascript"></script>
+        <script src="./js/script.js" type="text/javascript"></script>
+
+        <title>Mexican shooter</title>
+    </head>
+    <body>
+        <h1>Mexican shooter</h1>
+        <canvas id="gameBoard"></canvas>
+    </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function() {
+    $('canvas').drawImage({
+       source: '../Img/mexicain.png',
+       x: 20, y: 20,
+       scale: 0.25
+    });
+});
+
